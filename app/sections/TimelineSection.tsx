0a64fbdf795f3aa4d34e97841af525cec79ecfb1
royalkuriyakosem <royalkuriyakosem@gmail.com>
2026-01-01 21:32:10 +0530

    added some images

--- app/sections/TimelineSection.tsx
+++ app/sections/TimelineSection.tsx
@@ -37,9 +37,8 @@ const TimelineSection = () => {
                 }
             })
             .to(".ball02, .text01, #text-2020 ", {}, 0.04)
-            .to(".ball03, .text02, #text-2021 , #uoft", {}, 0.07)
-            .to(".ball04, .text03, #text-2024-jul", {}, 0.13)
-            .to(".ball05, .text04, #text-2024-sep", {}, 0.17)
+            .to(".ball03, .text02, #text-2022 , #uoft", {}, 0.07)
+            .to(".ball04, .text03, #text-2022-july, #fisat", {}, 0.13)
             .to(".ball06, .text05, #text-2024-dec, #chair, #haircut", {}, 0.2)
             .to(".ball07, #text-barbering-great, #competition, #pompodour, #combover", {}, 0.23)
             .to(".ball08, .text06, #text-2025-feb", {}, 0.42)
@@ -86,8 +85,16 @@ const TimelineSection = () => {
                 className='timeline-pic w-[50px] lg:w-[100px] 2xl:w-[150px] absolute top-[30%] left-[79%] md:top-[31%] md:left-[85%] lg:left-[73%] xl:top-[30%] xl:left-[70%] 2xl:top-[15%]  2xl:left-[65%] opacity-0 rotate-6'
             />
             <Image
+                id='fisat'
+                src='/fisat.jpg'
+                width={150}
+                height={150}
+                alt='fisat'
+                className='timeline-pic w-[50px] lg:w-[100px] 2xl:w-[150px] absolute top-[38%] left-[74%] md:top-[38%] md:left-[82%] lg:top-[37%] lg:left-[75%] xl:top-[37%] xl:left-[69%] 2xl:top-[29%] 2xl:left-[64%] opacity-0 -rotate-3'
+            />
+            <Image
                 id='chair'
-                src='/shop-3.jpg'
+                src='/chair.jpg'
                 width={120}
                 height={120}
                 alt='barber-chair'
@@ -173,8 +180,8 @@ const TimelineSection = () => {
                 <circle className="ball ball09" cx="281.8" cy="1992.2" r="20" />
                 <circle className="ball ball10" cx="430" cy="2191.1" r="20" />
                 <text className="text01" x="50" y="194.11">2020</text>
-                <text className="text02" x="50" y="394.11">2021</text>
-                <text className="text03" x="50" y="724.11">2024</text>
+                <text className="text02" x="50" y="394.11">2022</text>
+                <text className="text03" x="50" y="724.11">2022</text>
                 <text className="text04" x="50" y="994.11">2024</text>
                 <text className="text05" x="50" y="1186.31">2024</text>
                 <text className="text06" x="50" y="1786.31">2025</text>
@@ -183,19 +190,19 @@ const TimelineSection = () => {
             </svg>
 
             <div id='text-2020' className='timeline-text  absolute top-[26%] left-[42%] md:top-[27%] md:left-[40%] lg:left-[40%] xl:top-[27%] xl:left-[30%] 2xl:top-[7%] 2xl:left-[35%] ' >
-                <p>Completed Senior Secondary Education (12th) at St. Joseph's School with 90.80%</p>
+                <p>Completed Higher Secondary Education (10th) at St. Joseph's School with 90.80%</p>
             </div>
-            <div id='text-2021' className='timeline-text absolute top-[30%] left-[45%] md:top-[31%] md:left-[42%] lg:left-[33%] xl:top-[31%] xl:left-[30%] 2xl:top-[15%] 2xl:left-[37%]'>
-                <p>Started Bachelor of Technology in Computer Science at FISAT</p>
+            <div id='text-2022' className='timeline-text absolute top-[30%] left-[45%] md:top-[31%] md:left-[42%] lg:left-[33%] xl:top-[31%] xl:left-[30%] 2xl:top-[15%] 2xl:left-[37%]'>
+                <p>Completed Higher Secondary Education (12th) at St. Joseph's School with 92.20%</p>
             </div>
-            <div id='text-2024-jul' className='timeline-text absolute top-[36%] left-[33%] md:top-[38%] md:left-[30%] lg:left-[23%] xl:top-[38%] xl:left-[25%] 2xl:top-[29%] 2xl:left-[22%]'>
-                <p>Jul: Android App Development Intern - Developed Lab Entry Log app</p>
+            <div id='text-2022-july' className='timeline-text absolute top-[36%] left-[33%] md:top-[38%] md:left-[30%] lg:left-[23%] xl:top-[38%] xl:left-[25%] 2xl:top-[29%] 2xl:left-[22%]'>
+                <p>July:Started my Bachelors in Computer Science at FISAT</p>
             </div>
             <div id='text-2024-sep' className='timeline-text absolute top-[41%] left-[47%] md:top-[42%] md:left-[42%] lg:left-[33%] lg:top-[42%] xl:top-[42%] xl:left-[28%] 2xl:top-[36%] 2xl:left-[31%]'>
                 <p>Sep: Front-End Developer (Freelance) for Eventorgs - Built UI with React</p>
             </div>
             <div id='text-2024-dec' className='timeline-text absolute top-[45%] left-[50%] md:top-[45%] md:left-[46%] lg:left-[35%] lg:top-[45%] xl:top-[45%] xl:left-[32%] 2xl:top-[44%] 2xl:left-[40%]'>
-                <p>Dec: Front-End Intern at Nest Digital - Created Indian Tourism Website with Angular</p>
+                <p>Dec: Front-End Intern at Nest Digital  </p>
             </div>
             <div id='text-barbering-great' className='timeline-text absolute top-[50%] left-[43%] md:top-[49%]  md:left-[40%] lg:left-[35%] lg:top-[48%] xl:top-[49%] xl:left-[28%] 2xl:top-[50%] 2xl:left-[40%]'>
                 <p>Maintained a high CGPA (8.71) while gaining practical industry experience</p>
